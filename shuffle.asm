f26ac7484c8ab2840cf1e85145ca0619539b114a
Andrew <andrwww@utexas.edu>
2016-02-01 14:28:10 -0600

    changed some ldw, stw to ldb and stb

--- shuffle.asm
+++ shuffle.asm
@@ -1,7 +1,7 @@
 			.ORIG x3000
 			LEA R0, maskaddr
-			LDW R0, R0, #0
-			LDW R0, R0, #0 ; R0 = mask control
+			LDW R0, R0, #0 ; 
+			LDB R0, R0, #0 ; R0 = mask control
 			LEA R1, startaddr
 			LDW R1, R1, #0 ; R1 = x4000 (base addr)
 			AND R2, R2, #0 ; 
@@ -11,8 +11,8 @@
 
 again		AND R4, R0, #3 ; get rightmost 2 bits
 			ADD R5, R1, R4 ;
-			LDW R6, R5, #0 ; get memory content specified by mask control
-			STW R6, R3, #0
+			LDB R6, R5, #0 ; get memory content specified by mask control			
+			STB R6, R3, #0
 			ADD R3, R3, #1 ; incr store address
 			RSHFL R0, R0, #2
 			ADD R2, R2, #-1 ;decr counter
